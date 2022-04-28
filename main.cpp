ee7f910e04acabde0a267c91818bce269a7ba064
jwy <1141346781@qq.com>
2022-04-28 11:15:02 +0000

    main

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void ps1();
 void cd();
 void ls();
 
-string cwd = get_current_dir_name();
+string path = get_current_dir_name();
 
 typedef void (*DoFunc)();
 
@@ -59,7 +59,7 @@ int main()
 
 void pwd()
 {
-    cout << cwd << endl;
+    cout << path << endl;
 }
 string user(string a)
 {
@@ -82,9 +82,9 @@ void ps1()
     cout << user("name") << "@";
     cname();
     cout << ":";
-    if (cwd == user("dir")) cout << "~";
-    else if (cwd == "/") cout << "/";
-    else cout << cwd;
+    if (path == user("dir")) cout << "~";
+    else if (path == "/") cout << "/";
+    else cout << path;
     if (user("name") == "root") cout << " # ";
     else cout << " $ ";
 }
@@ -92,20 +92,20 @@ void cd()
 {
     string temp;
     cin >> temp;
-    string temp1 = cwd + "/" + temp;
+    string temp1 = path + "/" + temp;
     DIR *pDir;
-    if (temp == "~") cwd = user("dir");
-    else if (temp == ".") cwd = cwd;
-    else if (temp == "..") {for (int i=cwd.size()-1;i>=0;i--) if (cwd[i]=='/' and i != 0) {cwd=cwd.substr(0,i);break;} else if (cwd[i]=='/' and i == 0) {cwd="/";break;}}
-    else if (temp[0] == '/') if (!(pDir = opendir(temp.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else cwd = temp;
-    else if (!(pDir = opendir(temp1.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else cwd += "/" + temp;
-    if (cwd[0] == '/' and cwd[1] == '/') cwd = cwd.substr(1,cwd.size()-1);
+    if (temp == "~") path = user("dir");
+    else if (temp == ".") path = path;
+    else if (temp == "..") {for (int i=path.size()-1;i>=0;i--) if (path[i]=='/' and i != 0) {path=path.substr(0,i);break;} else if (path[i]=='/' and i == 0) {path="/";break;}}
+    else if (temp[0] == '/') if (!(pDir = opendir(temp.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else path = temp;
+    else if (!(pDir = opendir(temp1.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else path += "/" + temp;
+    if (path[0] == '/' and path[1] == '/') path = path.substr(1,path.size()-1);
 }
 void ls()
 {
     DIR *pDir;
     struct dirent* ptr;
-    if(!(pDir = opendir(cwd.c_str())))
+    if(!(pDir = opendir(path.c_str())))
     {
         return;
     }
