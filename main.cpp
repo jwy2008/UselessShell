bd1c7af413b466a3285049778645e08280e581f6
jwy <1141346781@qq.com>
2022-07-27 10:01:30 +0800

    哼(￢︿￢))

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-//function
+// function
 string user(string a);
 string cname();
 void config();
@@ -17,8 +17,8 @@ void echo();
 void help();
 void exit();
 
-//variable
-string path = get_current_dir_name(); //parh
+// variable
+string path = get_current_dir_name(); // parh
 
 typedef void (*DoFunc)();
 
@@ -29,16 +29,15 @@ struct TCommandMap
 };
 
 struct TCommandMap gCommandMaps[] =
-{
-    {"ls",ls},
-    {"pwd",pwd},
-    {"cd",cd},
-    {"echo",echo},
-    {"help",help},
-    {"exit",exit}
-};
+    {
+        {"ls", ls},
+        {"pwd", pwd},
+        {"cd", cd},
+        {"echo", echo},
+        {"help", help},
+        {"exit", exit}};
 
-int main() //main
+int main() // main
 {
     while (true)
     {
@@ -46,44 +45,50 @@ int main() //main
         config();
         cin >> input;
         int len = input.size();
-        if (input[len-1] == '\n') input[len-1] = '\0';
-        if (len == 0) continue;
+        if (input[len - 1] == '\n')
+            input[len - 1] = '\0';
+        if (len == 0)
+            continue;
         bool bFunc = false;
-        for (int i=0;i<sizeof(gCommandMaps)/sizeof(struct TCommandMap);i++)
+        for (int i = 0; i < sizeof(gCommandMaps) / sizeof(struct TCommandMap); i++)
         {
             char ch[32];
-            strcpy(ch,input.c_str());
-            if (strcasecmp(gCommandMaps[i].mCommand,ch) == 0)
+            strcpy(ch, input.c_str());
+            if (strcasecmp(gCommandMaps[i].mCommand, ch) == 0)
             {
-            bFunc = true;
-            gCommandMaps[i].mFunc();
-            break;
+                bFunc = true;
+                gCommandMaps[i].mFunc();
+                break;
             }
-	    }
-	    if (!(bFunc)) cout << "sh: command not found: " << input << endl;
+        }
+        if (!(bFunc))
+            cout << "sh: command not found: " << input << endl;
     }
 }
 
-string user(string a) //user name and user dir
+string user(string a) // user name and user dir
 {
     uid_t userid;
-    struct passwd* pwd;
-    userid=getuid();
+    struct passwd *pwd;
+    userid = getuid();
     pwd = getpwuid(userid);
-    if (a == "name") return pwd->pw_name;
-    else if (a == "dir") return pwd->pw_dir;
+    if (a == "name")
+        return pwd->pw_name;
+    else if (a == "dir")
+        return pwd->pw_dir;
     return "";
 }
 
-string cname() //computer name
+string cname() // computer name
 {
     char temp[32];
-    if(gethostname(temp,sizeof(temp))) perror("gethostname");
+    if (gethostname(temp, sizeof(temp)))
+        perror("gethostname");
     string name = temp;
     return name;
 }
 
-void config() //config (beautification)
+void config() // config (beautification)
 {
     /*---------------------------
         user("name"): user name
@@ -91,14 +96,19 @@ void config() //config (beautification)
         user("dir"): user dir
     ---------------------------*/
     cout << user("name") << "@" << cname() << ":";
-    if (path == user("dir")) cout << "~";
-    else if (path == "/") cout << "/";
-    else cout << path;
-    if (user("name") == "root") cout << "#";
-    else cout << "$";
+    if (path == user("dir"))
+        cout << "~";
+    else if (path == "/")
+        cout << "/";
+    else
+        cout << path;
+    if (user("name") == "root")
+        cout << "#";
+    else
+        cout << "$";
 }
 
-//directives
+// directives
 void pwd()
 {
     cout << path << endl;
@@ -110,31 +120,62 @@ void cd()
     cin >> temp;
     string temp1 = path + "/" + temp;
     DIR *pDir;
-    if (temp == "~") path = user("dir");
-    else if (temp == ".") path = path;
-    else if (temp == "..") {for (int i=path.size()-1;i>=0;i--) if (path[i]=='/' and i != 0) {path=path.substr(0,i);break;} else if (path[i]=='/' and i == 0) {path="/";break;}}
-    else if (temp[0] == '/') if (!(pDir = opendir(temp.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else path = temp;
-    else if (!(pDir = opendir(temp1.c_str()))) {cout << "cd: no such file or directory: " << temp << endl; return ;} else path += "/" + temp;
-    if (path[0] == '/' and path[1] == '/') path = path.substr(1,path.size()-1);
+    if (temp == "~")
+        path = user("dir");
+    else if (temp == ".")
+        path = path;
+    else if (temp == "..")
+    {
+        for (int i = path.size() - 1; i >= 0; i--)
+            if (path[i] == '/' and i != 0)
+            {
+                path = path.substr(0, i);
+                break;
+            }
+            else if (path[i] == '/' and i == 0)
+            {
+                path = "/";
+                break;
+            }
+    }
+    else if (temp[0] == '/')
+        if (!(pDir = opendir(temp.c_str())))
+        {
+            cout << "cd: no such file or directory: " << temp << endl;
+            return;
+        }
+        else
+            path = temp;
+    else if (!(pDir = opendir(temp1.c_str())))
+    {
+        cout << "cd: no such file or directory: " << temp << endl;
+        return;
+    }
+    else
+        path += "/" + temp;
+    if (path[0] == '/' and path[1] == '/')
+        path = path.substr(1, path.size() - 1);
 }
 
 void ls()
 {
     DIR *pDir;
-    struct dirent* ptr;
-    if(!(pDir = opendir(path.c_str())))
+    struct dirent *ptr;
+    if (!(pDir = opendir(path.c_str())))
     {
         return;
     }
-    while((ptr = readdir(pDir))!=NULL)
+    while ((ptr = readdir(pDir)) != NULL)
     {
         if (strcmp(ptr->d_name, ".") != 0 and strcmp(ptr->d_name, "..") != 0 and strncmp(ptr->d_name, ".", 1) != 0)
         {
-            if (ptr->d_type == DT_REG) cout << ptr->d_name << endl;
-            else if (ptr->d_type == DT_DIR) cout << ptr->d_name << "/" << endl;
+            if (ptr->d_type == DT_REG)
+                cout << ptr->d_name << endl;
+            else if (ptr->d_type == DT_DIR)
+                cout << ptr->d_name << "/" << endl;
         }
     }
-    closedir(pDir); 
+    closedir(pDir);
 }
 
 void echo()
@@ -146,7 +187,8 @@ void echo()
 
 void help()
 {
-    cout << "[Help]:" << endl << "cd echo exit help ls pwd" << endl;
+    cout << "[Help]:" << endl
+         << "cd echo exit help ls pwd" << endl;
 }
 
 void exit()
